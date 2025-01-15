1486d20f4a1125a513ad5eccc2fd262727584295
AnandKumarSinha07 <anandkr7808@gmail.com>
2025-01-15 17:22:08 +0530

    Added jsonweb token for security

--- src/users/userController.ts
+++ src/users/userController.ts
@@ -1,13 +1,16 @@
 import { NextFunction, Request,Response } from "express";
 import createHttpError from "http-errors";
 import user from "./userModel";
+import bcrypt from "bcrypt";
+import jwt from 'jsonwebtoken';
+
 
 
 const RegisterUser=async(req:Request,res:Response,next:NextFunction)=>{
    try {
 
        const {name,email,password}=req.body;
-       console.log("body of the user is",req.body)
+       
 
        if(!name || !email || !password){
           const error=createHttpError(404,"Enter all the required fields")
@@ -21,16 +24,31 @@ const RegisterUser=async(req:Request,res:Response,next:NextFunction)=>{
           return next(error)
        }
 
+       const hashPassword=await bcrypt.hash(password,10);
+
+       if(!hashPassword){
+          const error=createHttpError(500,"password hashing not working properly")
+          return next(error)
+       }
 
 
 
+       const newUser=await user.create({
+        name:name,
+        email:email,
+        password:hashPassword
+       })
 
 
+       const token=jwt.sign({sub:newUser._id},"anand123@",{
+           expiresIn:'7d'
+       })
 
-
+        
 
        res.status(200).json({
-        message:"User Register"
+        message:"User Register",
+        accesToken:token,
        })
 
 
