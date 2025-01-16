1f4bc788c741ad231104249d3695d83a7b557910
AnandKumarSinha07 <anandkr7808@gmail.com>
2025-01-16 23:05:04 +0530

    Added file and pdf in the cloudinary

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -1,11 +1,43 @@
 import { NextFunction ,Request,Response} from "express";
+import path from "node:path";
+import cloudinary from "../config/cloudinary";
 import book from "./bookModel";
 
 
 const createBook=async(req:Request,res:Response,next:NextFunction)=>{
    try {
-     
+        
+    console.log("req",req.files)
 
+     const files=req.files as {[filename:string]:Express.Multer.File[]}   
+ 
+     const coverImgMimetype=files.coverImg[0].mimetype.split("/").at(-1)
+     const filename=files.coverImg[0].filename;
+     const filePath=path.resolve(__dirname,'../../public/data/uploads',filename)
+
+     const uploadResult=await cloudinary.uploader.upload(filePath,{
+         filename_override:filename,
+         folder:"book-cover",
+         format:coverImgMimetype,
+     })
+
+     const bookfileName=files.file[0].filename;
+     const bookFilePath=path.resolve(__dirname,'../../public/data/uploads',filename)
+
+     const bookFileUpload=await cloudinary.uploader.upload(bookFilePath,{
+         resource_type:'raw',
+         filename_override:bookfileName,
+         folder:'book-pdfs',
+         format:"pdf"
+     })
+
+     console.log("fileUpload",bookFileUpload)
+
+
+     console.log("upload result",uploadResult)
+     res.json({
+       message:"ok"
+     })
    } catch (error) {
     
    }
---
+++ src/config/cloudinary.ts
@@ -0,0 +1,12 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+
+
+
+cloudinary.config({ 
+    cloud_name: 'dpheetc0h', 
+    api_key: '917649638998442', 
+    api_secret: 'sfpyu2P10OAKNsSlUOeKgdQ7AwA' 
+});
+
+export default cloudinary;
